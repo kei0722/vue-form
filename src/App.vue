044ff165033ca927be13e117e16955c5abbf40da
kei0722 <kayare919@gmail.com>
2021-03-21 15:36:59 +0900

    基本情報入力画面、アンケート画面、相談内容入力画面を実装

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+		<div class="container">
+			<div class="inner-container">
+				<div class="header-section">
+					<h3>STEP{{pageNumber}}</h3>
+					<h2>{{ title }}</h2>
+				</div>
+				<router-view @title="title = $event" @nextPage="nextPage = $event" @priviousPage="priviousPage = $event"></router-view>
+			</div>
+			<div class="btn-section">
+				<router-link :to="priviousPage" @click.native="minusPageNum" v-if="!(pageNumber === 1)" class="privious-btn">&lt; 前へ戻る</router-link>
+				<router-link :to="nextPage" @click.native="addPageNum" class="next-btn">次へ進む &gt;</router-link>
+			</div>
+	</div>
+
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			pageNumber: 1,
+			title: '',
+			priviousPage: '',
+			nextPage: ''
+		}
+	},
+	methods: {
+		minusPageNum() {
+			if (this.pageNumber > 1) {
+				this.pageNumber--;
+			}
+		},
+		addPageNum() {
+			if (this.pageNumber < 3) {
+				this.pageNumber++;
+			}
+		}
+	}
+}
+</script>
+
+<style>
+body {
+	font-family: "游ゴシック", "Yu Gothic", "游ゴシック体", "YuGothic", sans-serif;
+}
+
+.inner-container {
+	border: 1px solid #2cdcb8;
+	border-radius: 5px;
+}
+
+.header-section {
+	border-bottom: 1px solid #2cdcb8;
+	position: relative;
+	background-color: #abdee6;
+}
+
+h2 {
+	margin: 0;
+	padding: 10px 0;
+	text-align: center;
+}
+
+h3 {
+	margin: 0;
+	font-weight: normal;
+	font-size: 16px;
+	padding: 3px;
+	position: absolute;
+	top: 0;
+	left: 0;
+	background-color: rgb(70 156 255);
+	color: rgb(255 255 255);
+	border-radius: 3px;
+}
+
+.form-section {
+	padding: 20px;
+}
+
+.question-title {
+	color: #1d93c3;
+}
+
+.question-title > p {
+	margin: 0 0 10px 0;
+}
+
+.question-answer {
+	margin-left: 5px;
+}
+
+.question:not(:first-child) {
+	margin-top: 30px;
+}
+
+select {
+	font-size: 16px;
+	padding: 5px;
+	width: 100px;
+}
+
+.btn-section {
+	margin-top: 20px;
+	display: flex;
+	justify-content: space-around;
+}
+
+.privious-btn,
+.next-btn {
+	border: 1px solid #2cdcb8;
+	border-radius: 3px;
+	background-color: #2cdcb8;
+	color: white;
+	text-decoration: none;
+	padding: 7px 30px;
+}
+</style>
---
+++ src/components/UserQuestions.vue
@@ -0,0 +1,83 @@
+<template>
+<div>
+	<div class="form-section">
+		<div class="question">
+			<div class="question-title">
+				<p>現在、生命保険に加入されていますか？</p>
+			</div>
+			<div class="question-answer">
+				<input type="radio" id="question1-y" value="yes" v-model="formData2.question1">
+				<label for="question1-y">はい</label>
+				<input type="radio" id="question1-n" value="no" v-model="formData2.question1">
+				<label for="question1-n">いいえ</label>
+			</div>
+		</div>
+
+		<transition name="fade">
+		<div class="question" v-if="formData2.question1">
+			<div class="question-title">
+				<p>現在入院中ですか？または、最近3ヶ月以内に医師の診断・審査の結果、入院・手術を勧められたことはありますか？</p>
+			</div>
+			<div class="question-answer">
+				<input type="radio" id="question2-y" value="yes" v-model="formData2.question2">
+				<label for="question2-y">はい</label>
+				<input type="radio" id="question2-n" value="no" v-model="formData2.question2">
+				<label for="question2-n">いいえ</label>
+			</div>
+		</div>
+		</transition>
+
+		<transition name="fade">
+		<div class="question" v-if="formData2.question2">
+			<div class="question-title">
+				<p>過去5年以内に、病気やけがで、手術を受けたことまたは継続して7日以上の入院をしたことがありますか？</p>
+			</div>
+			<div class="question-answer">
+				<input type="radio" id="question3-y" value="yes" v-model="formData2.question3">
+				<label for="question3-y">はい</label>
+				<input type="radio" id="question3-n" value="no" v-model="formData2.question3">
+				<label for="question3-n">いいえ</label>
+			</div>
+		</div>
+		</transition>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: '以下にお答えください',
+			priviousPage: '/',
+			nextPage: '/user-textbox',
+			show: false,
+			formData2: {
+				question1: '',
+				question2: '',
+				question3: ''
+			}
+		}
+	},
+	created() {
+		this.$emit('title', this.title);
+		this.$emit('priviousPage', this.priviousPage);
+		this.$emit('nextPage', this.nextPage);
+	}
+}
+</script>
+
+<style scoped>
+	.fade-enter,
+	.fade-leave-to {
+		opacity: 0;
+	}
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity .5s;
+	}
+	.fade-enter-to,
+	.fade-leave {
+		opacity: 1;
+	}
+</style>
